f7df88954eae1cc821134d8202e28f343ca88e33
Edward Lay <edwardlay@edwards-mac-mini.local>
2018-11-18 17:04:09 +1100

    Initial Commit

---
+++ ButtonTap/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var buckysLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        //sender is the object that called this method (button)
+        
+        let title = sender.titleForState(.Normal)!
+        
+        buckysLabel.text = "You clicked the \(title) button"
+        
+    }
+    
+}
+
